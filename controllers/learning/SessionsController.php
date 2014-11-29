00818f635caf0cbb58ec960de9dd58c6a6e742bf
UltraDmin <UltraDmin@UltraDmin-GPM>
2014-11-29 22:28:11 +0200

    Прошел сессии - перехожу к кукам

--- controllers/learning/SessionsController.php
+++ controllers/learning/SessionsController.php
@@ -8,12 +8,50 @@ namespace app\controllers\learning;
 class SessionsController
     extends \yii\web\Controller
 {
-    public function actionIndex()
+    public function actionRequests()
     {
-        $session = \Yii::$app->session;
+        $request = \Yii::$app->request;
         
-        $res = $session->set("slava", "This is session value");
-        $session->close();
-        $res = $session->get("slava");
+        $request->get(); //  = $_GET;
+        $request->get("id"); // $_GET["id"];
+        
+        // возвращает true / false
+        $request->isAjax;
+        $request->isGet;
+        $request->isPost;
+        
+        
+    }
+
+    public function actionЫуыышщты()
+    {
+        $session = \Yii::$app->session; //получаем объект с сессиями
+        
+        $session->set("slava", "This is session value"); //установка сессии
+        //
+        // закрывает сессию, но при использовании get / set сессия открывается автоматически
+        $session->close(); 
+        $res = $session->get("slava"); //получение данных с сессии.
+        
+        $session->remove("slava");
+        
+        // ввиду ограничений эти две строки работать не будут.
+        $session["slava"]["name"] = "slava";
+        $session["slava"]["last_name"] = "bla bla bla";
+        
+        // а это рабочий код.
+        $session["slava"] = 
+        [
+            "name" => "slava",
+            "'ast_name" => "bla bla bla",
+        ];
+        
+        // или
+        
+       $slava = $session["slava"];
+       $slava["name"] = "sds";
+       $slava["last_name"] = "ghfgh";
+        
+        print $session["slava"]["name"]; //этот кодж будет работать.
     }
 }
